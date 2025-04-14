d53c6218e1862b5fc79f402b406d39acfc0ebd35
Pietro Valente <pietro.valente@yahoo.com>
2025-04-14 14:42:08 +0100

    Update cross-platform version

--- src/Connection.h
+++ src/Connection.h
@@ -3,7 +3,28 @@
 #include <openssl/err.h>
 #include <openssl/ssl.h>
 
-#pragma comment(lib, "ws2_32.lib")
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #pragma comment(lib, "Ws2_32.lib")
+    typedef SOCKET socket_t;
+    #define CLOSE_SOCKET closesocket
+    #define SOCKET_INIT() WSAStartup(MAKEWORD(2, 2), &wsaData)
+    #define SOCKET_CLEANUP() WSACleanup()
+    #define INVALID_SOCKET_VAL INVALID_SOCKET
+#else
+    #include <unistd.h>
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netdb.h>
+    #include <arpa/inet.h>
+    #include <netinet/in.h>
+    typedef int socket_t;
+    #define CLOSE_SOCKET close
+    #define SOCKET_INIT() 0
+    #define SOCKET_CLEANUP() do {} while (0)
+    #define INVALID_SOCKET_VAL -1
+#endif
 
 class Connection {
 public:
@@ -17,6 +38,6 @@ private:
     std::string port;
     SSL_CTX* ctx;
     SSL* ssl;
-    SOCKET sockfd;
+    socket_t sockfd;
     struct hostent* server;
 };
